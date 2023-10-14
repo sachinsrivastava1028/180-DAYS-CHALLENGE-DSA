e72d6a60d9a8240d40c6d96d5aea7aefa8112912
sachinsrivastava1028 <sachinsrivastava1028@gmail.com>
2023-10-14 22:48:35 +0530

    code

---
+++ 8.While loop/1tonprint.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of 'n': ";
+    cin>>n;
+    
+    // 1 to n
+    int i=1;
+    while(i<=n)
+    {
+        cout<<i<<" ";
+        i=i+1;
+    }
+}
